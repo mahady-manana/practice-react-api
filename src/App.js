a256d61ab678fc045361d88e2bf23d2922e21e50
Nelly <faranomenafanampy@gmail.com>
2020-05-15 14:00:54 +1000

    Flux de données unidirectionnel

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{Component} from 'react';
 import './App.css';
+const list = [
+  {
+    title: 'React',
+    url: 'https ://facebook.github.io/react/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'React',
+    url: 'https ://facebook.github.io/react/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 1,
+  }
+];
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      list: list,
+    };
+    //
+    //this.onDismiss = this.onDismiss.bind(this);
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  onDismiss(id) {
+    const isNotId = item => item.objectID !== id;
+
+    const updatedList = this.state.list.filter(isNotId);
+    this.setState({ list: updatedList });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {
+          this.state.list.map(item =>
+            <div key={item.objectID}>
+              <span>
+                <a href={item.url}>{item.title}</a>
+              </span>
+              <span>{item.author}</span>
+              <span>{item.num_comments}</span>
+              <span>{item.points}</span>
+              <span>
+                <button
+                  onClick={() => this.onDismiss(item.objectID)}
+                  type="button"
+                >
+                  Dismiss
+                </button>
+              </span>
+            </div>
+          )
+        }
+      </div>
+    )
+  }
 }
 
 export default App;
