55746f58bee276ab5a728e411f342da006bb58a5
Armel <arbandry@gmail.com>
2020-05-18 22:25:14 +0300

    add component clock

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React,{Component} from 'react';  
 import Search from './components/Search';
 import './App.css';
+import Clock from './components/Clock';
 class App extends Component {  
   render() { 
     return (
       <div className="App">
+        <Clock/>
         <Search>
           tadiavo : 
         </Search>   
